1c64fa5341a666df37a4261697a00660057b2224
moiseevanton <athidif94@gmail.com>
2016-05-06 13:36:06 -0400

    Initial Commit

---
+++ PigLatin/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  PigLatin
+//
+//  Created by Anton Moiseev on 2016-05-06.
+//  Copyright © 2016 Anton Moiseev. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
